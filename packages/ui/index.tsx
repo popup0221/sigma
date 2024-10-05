88d27e83097d3282ff9c16799857439b8d3d8714
popup0221 <popup0221@naver.com>
2024-10-05 22:00:00 +0900

    chore: 파일 위치 변경

---
+++ packages/ui/index.tsx
@@ -0,0 +1,8 @@
+// components
+
+// shadcn components
+export * from '@/components/ui/button';
+export * from '@/components/ui/drawer';
+
+// utils
+export { cn } from '@/lib/utils';
--- packages/ui/postcss.config.js
+++ packages/ui/postcss.config.js
@@ -1,6 +1,2 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+//packages/ui/postcss.config.js
+module.exports = require("@repo/tailwind-config/postcss.config")
